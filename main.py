29e0ac12ab8c71e851ac7b54854022009333cbcf
Marco Pellizzaro <83591116+MarcoPellizzaro@users.noreply.github.com>
2022-11-16 13:32:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-from bs4 import BeautifulSoup
+# Marco Vinícius Costódio Pellizzaro
+'''
+ENUNCIADO 
+Sua  tarefa  será  transformar  um  conjunto  de  5  sites,  sobre  o  tema  de  processamento  de 
+linguagem natural em um conjunto de cinco listas distintas de sentenças. Ou seja, você fará uma função 
+que, usando a biblioteca Beautifull Soap, faça a requisição de uma url, e extrai todas as sentenças desta 
+url. Duas condições são importantes:  
+a) A página web (url) deve apontar para uma página web em inglês contendo, não menos que 
+1000 palavras.  
+b) O texto desta página deverá ser transformado em um array de senteças.   
+'''
 import requests
-import os
-import re
+from bs4 import BeautifulSoup, Comment
+import spacy
 
-url = 'https://en.wikipedia.org/wiki/Parsing'
-res1 = requests.get(url)
-url = 'https://academic.oup.com/jamia/article/18/5/544/829676?login=false'
-res2 = requests.get(url)
-url = 'https://en.wikipedia.org/wiki/Natural_language_processing'
-res3 = requests.get(url)
-url = 'https://www.techtarget.com/searchenterpriseai/definition/natural-language-processing-NLP'
-res4 = requests.get(url)
-url = 'https://www.datarobot.com/blog/what-is-natural-language-processing-introduction-to-nlp/'
-res5 = requests.get(url)
-html_pages = {res1.text, res2.text, res3.text, res4.text, res5.text}
+nlp = spacy.load("en_core_web_sm") 
 
-corpus = []
+url1 = 'https://en.wikipedia.org/wiki/Natural_language_processing'
+url2 = 'https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html'
+url3 = 'https://www.techtarget.com/searchenterpriseai/definition/natural-language-processing-NLP'
+url4 = 'https://towardsdatascience.com/your-guide-to-natural-language-processing-nlp-48ea2511f6e1'
+url5 = 'https://www.datarobot.com/blog/what-is-natural-language-processing-introduction-to-nlp/'
 
-for html_page in html_pages:
-  soup = BeautifulSoup(html_page, 'html.parser')
-  linhas = re.split('[.!?\n]',soup.get_text())
-  corpus.append(linhas)
+sites = [url1, url2, url3, url4, url5]
 
-while True:
-  _ = os.system('clear')
-  print("Pesquisar")
-  pesquisa = input(">>")
+textos = []
 
-  _ = os.system('clear')
-  print("Pesquisando: " + pesquisa)
+i = 0
+while i < 5:
+  texto = []
+  textos.append(texto)
+  i += 1
 
-  sair = False
-  for i in corpus:
-    for j in i:
-      if pesquisa in j.lower():
-        print(j)
-        entrada = input(">>ENTER para continuar procurando\n>>digite sair para sair\n>>")
-        os.system('clear')
-        print("Pesquisando: " + pesquisa)
-        
-        if entrada.lower() == "sair":
-          sair = True
-          break
-    if sair:
-      break
+i = 0;
+while i < 5:                     
+  html = requests.get(sites[i]).text                     
+  soup = BeautifulSoup(html, 'html.parser')     
+  for j in soup(['style', 'script', 'head', 'header', 'meta', '[document]', 'title', 'footer', 'iframe', 'nav']):
+    j.decompose()
+    texts = ' '.join(soup.stripped_strings)                      
+  page = nlp(texts)
+
+  for x in page.sents:
+      textos[i].append(x.text)
+
+  i += 1;
+
+for i in textos:
+  print(i)
